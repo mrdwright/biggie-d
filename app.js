a72137028cad5e0d71cca3583626471c9677f7e7
mrdwright <mrdwright@shaw.ca>
2017-11-07 11:44:17 -0800

    adding LUIS and QnA Maker

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ A simple echo bot for the Microsoft Bot Framework.
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var cognitiveservices = require('botbuilder-cognitiveservices');
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-  
+
+// For Emulator to work
+// process.env.MicrosoftAppId = '';
+// process.env.MicrosoftAppPassword = '';
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
@@ -18,6 +23,14 @@ var connector = new builder.ChatConnector({
     stateEndpoint: process.env.BotStateEndpoint,
     openIdMetadata: process.env.BotOpenIdMetadata 
 });
+
+var qnarecognizer = new cognitiveservices.QnAMakerRecognizer({
+	knowledgeBaseId: '', 
+	subscriptionKey: '',
+    top: 4});
+
+var model = '{LUIS}';
+var recognizer = new builder.LuisRecognizer(model);
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
@@ -29,6 +42,24 @@ server.post('/api/messages', connector.listen());
 * ---------------------------------------------------------------------------------------- */
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+var bot = new builder.UniversalBot(connector);
+
+var intents = new builder.IntentDialog({ recognizers: [recognizer, qnarecognizer] });
+bot.dialog('/', intents);
+
+intents.matches('Weather.GetCondition', builder.DialogAction.send('Inside LUIS Intent 1.'));
+
+intents.matches('Weather.GetForecast', builder.DialogAction.send('Inside LUIS Intent 2.'));
+
+intents.matches('qna', [
+    function (session, args, next) {
+        var answerEntity = builder.EntityRecognizer.findEntity(args.entities, 'answer');
+        session.send(answerEntity.entity);
+    }
+]);
+
+intents.onDefault([
+    function(session){
+        session.send('Sorry!! No match!!');
+	}
+]);

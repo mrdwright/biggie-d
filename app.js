e5188a43b2f1219bc2b382d094e10ffa9bdb4e8e
mrdwright <mrdwright@shaw.ca>
2017-11-08 08:33:38 -0800

    adding bot handoff

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 /*-----------------------------------------------------------------------------
 A simple echo bot for the Microsoft Bot Framework. 
 -----------------------------------------------------------------------------*/
-
-var restify = require('restify');
+var express = require("express");
+//var restify = require('restify');
 var builder = require('botbuilder');
 var cognitiveservices = require('botbuilder-cognitiveservices');
+var handoff = require("botbuilder-handoff");
+//=========================================================
+// Normal Bot Setup
+//=========================================================
+// Setup Express Server (N.B: If you are already using restify for your bot, you will need replace it with an express server)
+var app = express();
+// Setup Express Server
+app.listen(process.env.port || process.env.PORT || 3978, '::', function () {
+    console.log('Server Up');
+});
+
+
+
 
 // Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
+// var server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, function () {
+//    console.log('%s listening to %s', server.name, server.url); 
+// });
 
 // For Emulator to work
 // process.env.MicrosoftAppId = '';
 // process.env.MicrosoftAppPassword = '';
+process.env.MongoDb
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
@@ -25,15 +39,17 @@ var connector = new builder.ChatConnector({
 });
 
 var qnarecognizer = new cognitiveservices.QnAMakerRecognizer({
-	knowledgeBaseId: '', 
-	subscriptionKey: '',
+	knowledgeBaseId: process.env.QnaKbId, 
+	subscriptionKey: process.env.QnaSubKey,
     top: 4});
 
-var model = '{LUIS}';
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/' + process.env.LuisAppId;
+model += '?subscription-key=' + process.env.LuisSubKey + '&verbose=true&timezoneOffset=0&q=';
 var recognizer = new builder.LuisRecognizer(model);
 
 // Listen for messages from users 
-server.post('/api/messages', connector.listen());
+//server.post('/api/messages', connector.listen());
+app.post('/api/messages', connector.listen());
 
 /*----------------------------------------------------------------------------------------
 * Bot Storage: This is a great spot to register the private state storage for your bot. 
@@ -63,3 +79,33 @@ intents.onDefault([
         session.send('Sorry!! No match!!');
 	}
 ]);
+
+//=========================================================
+// Handoff Setup
+//=========================================================
+ 
+// Replace this functions with custom login/verification for agents
+var isAgent = function (session) { return session.message.user.name.startsWith("Agent"); };
+
+/**
+   bot: builder.UniversalBot
+   app: express ( e.g. const app = express(); )
+   isAgent: function to determine when agent is talking to the bot
+   options: { }     
+**/
+handoff.setup(bot, app, isAgent, {
+   mongodbProvider: process.env.MONGODB_PROVIDER,
+   directlineSecret: process.env.MICROSOFT_DIRECTLINE_SECRET,
+   textAnalyticsKey: process.env.CG_SENTIMENT_KEY,
+   appInsightsInstrumentationKey: process.env.APPINSIGHTS_INSTRUMENTATIONKEY,
+   retainData: process.env.RETAIN_DATA,
+   customerStartHandoffCommand: process.env.CUSTOMER_START_HANDOFF_COMMAND
+});
+
+//triggerHandoff manually
+bot.dialog('/connectToHuman', function (session) {
+    session.send("Hold on, buddy! Connecting you to the next available agent!");
+    handoff.triggerHandoff(session);
+}).triggerAction({
+    matches: /^agent/i
+});

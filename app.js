29fcf0469893d8467e91c5a0aa6694fd74873e60
mrdwright <mrdwright@shaw.ca>
2017-11-27 09:16:26 -0800

    removing handoff

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require("express");
 var request = require('request');
 var builder = require('botbuilder');
 var cognitiveservices = require('botbuilder-cognitiveservices');
-var handoff = require("botbuilder-handoff");
+// var handoff = require("botbuilder-handoff");
 
 var weatherEndPoint = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%27Vancouver%27)%20and%20u=%27c%27&format=json';    
 
@@ -53,14 +53,14 @@ var luisrecognizer = new builder.LuisRecognizer('https://westus.api.cognitive.mi
 //   }     
 // )
 //=========================================================
-const isAgent = (session) => session.message.user.name.startsWith("agent");
+// const isAgent = (session) => session.message.user.name.startsWith("agent");
 
-handoff.setup(bot, app, isAgent, {
-    mongodbProvider: process.env.MONGODB_PROVIDER,
-    directlineSecret: process.env.MICROSOFT_DIRECTLINE_SECRET,
-    retainData: "true",
-    customerStartHandoffCommand: "human"
- });
+// handoff.setup(bot, app, isAgent, {
+//     mongodbProvider: process.env.MONGODB_PROVIDER,
+//     directlineSecret: process.env.MICROSOFT_DIRECTLINE_SECRET,
+//     retainData: "true",
+//     customerStartHandoffCommand: "human"
+//  });
 
 //========================================================
 // Bot Intents
@@ -98,8 +98,8 @@ intents.onDefault([
 //========================================================
 bot.dialog('/', intents);
 
-bot.dialog('/connectToHuman', function (session) {        
-    handoff.triggerHandoff(session);
-}).triggerAction({
-    matches: /^agent/i
-});
+// bot.dialog('/connectToHuman', function (session) {        
+//     handoff.triggerHandoff(session);
+// }).triggerAction({
+//     matches: /^agent/i
+// });
